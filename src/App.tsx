ed9dce2aeb69a951fed99c24c1e5138263e1fcee
GitBrowserBit <browsergit@gmail.com>
2024-12-15 09:30:16 -0300

    Projeto inicial:>> formulario

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Container, Stack } from '@mui/material'
+import './styles/App.css'
+
+function App() {
+  return (
+     <Container className='Container'>
+        <Container>
+          <Stack>
+            <span>Register</span>
+          </Stack>
+        </Container>
+     </Container>
+  )
+}
+
+export default App
